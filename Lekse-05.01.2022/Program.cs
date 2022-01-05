0ce32abc408c4b63006e76f3bedb5def82568100
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 09:58:36 +0100

    Add project files.

---
+++ Lekse-05.01.2022/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static Random Random = new Random();
+    static void Main(string[] args)
+    {
+        var randomLetters = new char[10];
+        var randomNumbers = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            var randomLetter = GetRandomUpperCaseLetter();
+            var randomNumber = Random.Next(0, 10);
+
+            randomLetters[i] = randomLetter;
+            randomNumbers[i] = randomNumber;
+        }
+    }
+
+    private static char GetRandomUpperCaseLetter()
+    {
+        return GetRandomLetter('A', 'Z');
+    }
+
+
+    private static char GetRandomLetter(char min, char max)
+    {
+        return (char)Random.Next(min, max);
+    }
+}
